cef7f3cbd5afecbb77262df1006befa036e1156a
abhaysp95 <abhaysp95@protonmail.com>
2024-03-21 21:26:03 +0530

    feat: changed default colorscheme and added 'trouble'

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,15 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  -- "LunarVim/darkplus.nvim",
+	-- 'ChristianChiarulli/nvcode-color-schemes.vim',
+	-- 'savq/melange-nvim',
+  'folke/tokyonight.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "tokyonight-night"
+  vim.opt.background = 'dark'
 end
 
 return M
