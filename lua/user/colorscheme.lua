501e221a8d3fcb423a85ff9154590138627bf394
abhaysp95 <abhaysp95@protonmail.com>
2024-04-11 21:12:18 +0530

    feat: changed to hacker colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,13 +2,14 @@ local M = {
   -- "LunarVim/darkplus.nvim",
 	-- 'ChristianChiarulli/nvcode-color-schemes.vim',
 	-- 'savq/melange-nvim',
-  'folke/tokyonight.nvim',
+  -- 'folke/tokyonight.nvim',
+  'RRethy/base16-nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "tokyonight-night"
+  vim.cmd.colorscheme "base16-greenscreen"
   vim.opt.background = 'dark'
 end
 
