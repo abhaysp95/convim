fccb6ab3678b86ecb84ec6f67d3c9ed954cb1dd5
abhaysp95 <abhaysp95@protonmail.com>
2024-06-27 21:39:01 +0530

    chore: updated colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,13 +3,14 @@ local M = {
 	-- 'ChristianChiarulli/nvcode-color-schemes.vim',
 	-- 'savq/melange-nvim',
   -- 'folke/tokyonight.nvim',
-  'RRethy/base16-nvim',
+  -- 'RRethy/base16-nvim',
+  'EdenEast/nightfox.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "base16-greenscreen"
+  vim.cmd.colorscheme "carbonfox"
   vim.opt.background = 'dark'
 end
 
