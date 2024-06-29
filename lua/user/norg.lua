ac7bddb3f17433aeb20c1802bac573986567b9ce
abhaysp95 <abhaysp95@protonmail.com>
2024-06-29 22:08:27 +0530

    feat: updating the neorg build system and adding workspaces etc.

--- lua/user/norg.lua
+++ lua/user/norg.lua
@@ -1,25 +1,43 @@
 local M = {
   "nvim-neorg/neorg",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  build = ":Neorg sync-parsers",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "vhyrro/luarocks.nvim",
+      priority = 1000,
+      config = true,
+    }
+  },
   -- tag = "*",
   lazy = true, -- enable lazy load
   ft = "norg", -- lazy load on file type
   cmd = "Neorg", -- lazy load on command
 }
 
-function M.config() 
+function M.config()
   require("neorg").setup {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },
+      ["core.concealer"] = {
+        config = {
+          icon_preset = "basic",  -- basic (default), diamond, varied
+        },
+      }, -- Adds pretty icons to your documents
       ["core.dirman"] = { -- Manages Neorg workspaces
         config = {
           workspaces = {
+            finance = "~/neorg/finance",
             personal = "~/neorg/personal_notes",
             work = "~/neorg/work",
             proj_ideas = "~/neorg/proj_ideas",
+            quick_notes = "~/neorg/quick_notes",
           },
+          default_workspace = "quick_notes",
         },
       },
     },
