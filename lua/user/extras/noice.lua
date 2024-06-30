264b85865dded1917509e48e2529b902918f0da9
abhaysp95 <abhaysp95@protonmail.com>
2024-06-30 11:12:59 +0530

    feat: get nice ui for different types of notifications
        sometimes it can get overwhelming

---
+++ lua/user/extras/noice.lua
@@ -0,0 +1,72 @@
+local M = {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  }
+}
+
+function M.config()
+  require("noice").setup({
+    lsp = {
+      progress = {
+        enabled = true,
+        -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+        -- See the section on formatting for more details on how to customize.
+        --- @type NoiceFormat|string
+        format = "lsp_progress",
+        --- @type NoiceFormat|string
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30, -- frequency to update lsp progress message
+        view = "mini",
+      },
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    config = {
+      notify = {
+        enabled = true,
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+    messages = {
+      -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+      -- This is a current Neovim limitation.
+      enabled = true, -- enables the Noice messages UI
+      view = "notify", -- default view for messages
+      view_error = "notify", -- view for errors
+      view_warn = "notify", -- view for warnings
+      view_history = "messages", -- view for :messages
+      view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+    },
+    popupmenu = {
+      enabled = true, -- enables the Noice popupmenu UI
+      ---@type 'nui'|'cmp'
+      backend = "nui", -- backend to use to show regular cmdline completions
+      ---@type NoicePopupmenuItemKind|false
+      -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+      kind_icons = {}, -- set to `false` to disable icons
+    },
+  })
+end
+
+return M;
