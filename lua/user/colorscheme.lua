3a851598dbb4e03f803690ab67c873854dd2507c
abhaysp95 <abhaysp95@protonmail.com>
2024-07-01 12:51:43 +0530

    feat: some new colorschemes to try out

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,13 +4,16 @@ local M = {
 	-- 'savq/melange-nvim',
   -- 'folke/tokyonight.nvim',
   -- 'RRethy/base16-nvim',
-  'EdenEast/nightfox.nvim',
+  -- 'EdenEast/nightfox.nvim',
+  -- 'NTBBloodbath/sweetie.nvim',
+  'rebelot/kanagawa.nvim',
+  -- 'nyoom-engineering/oxocarbon.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "carbonfox"
+  vim.cmd.colorscheme "kanagawa-wave"
   vim.opt.background = 'dark'
 end
 
