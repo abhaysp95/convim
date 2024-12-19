85073276278e9bae11211f990969ab6be2b1cddb
abhaysp95 <abhaysp95@protonmail.com>
2024-12-19 19:13:54 +0530

    feat: modified lualine & colorscheme changed

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,26 +1,33 @@
 local M = {
-  -- "LunarVim/darkplus.nvim",
+	-- "LunarVim/darkplus.nvim",
 	-- 'ChristianChiarulli/nvcode-color-schemes.vim',
 	-- 'savq/melange-nvim',
-  -- 'folke/tokyonight.nvim',
-  -- 'RRethy/base16-nvim',
-  'bluz71/vim-moonfly-colors',
-  -- 'mcchrish/zenbones.nvim',
-  -- dependencies = {
-  --   "rktjmp/lush.nvim"
-  -- },
-  -- 'EdenEast/nightfox.nvim',
-  -- 'NTBBloodbath/sweetie.nvim',
-  -- 'rebelot/kanagawa.nvim',
-  -- 'nyoom-engineering/oxocarbon.nvim',
-  -- 'oxfist/night-owl.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+	-- 'folke/tokyonight.nvim',
+	-- 'RRethy/base16-nvim',
+	-- 'bluz71/vim-moonfly-colors',
+	-- 'ellisonleao/gruvbox.nvim',
+	"sainnhe/gruvbox-material",
+	-- 'mcchrish/zenbones.nvim',
+	-- dependencies = {
+	--   "rktjmp/lush.nvim"
+	-- },
+	-- 'EdenEast/nightfox.nvim',
+	-- 'NTBBloodbath/sweetie.nvim',
+	-- "rebelot/kanagawa.nvim",
+	-- 'nyoom-engineering/oxocarbon.nvim',
+	-- 'oxfist/night-owl.nvim',
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "moonfly"
-  vim.opt.background = 'dark'
+	-- require("gruvbox").setup({
+	-- 	contrast = "hard",
+	-- })
+	vim.cmd.colorscheme("gruvbox-material")
+	vim.opt.background = "dark"
+	vim.g.moonflyItalics = false
+	vim.cmd("highlight Normal ctermbg=none guibg=none")
 end
 
 return M
